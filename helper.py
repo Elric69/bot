2c387f1b7bea73c850bc3ae27593671072b0dc9c
Elric69 <97374362+Elric69@users.noreply.github.com>
2023-01-31 12:14:41 +0530

    Add files via upload

---
+++ helper.py
@@ -0,0 +1,95 @@
+hp = """
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+[C]Bot is online!!
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+-> image [image commands]
+-> common [common commands]
+-> game [About game]
+-> admin [admin commands]
+-> iadmin [admin names]
+[c]----------------------------
+
+Note -> Before you use command make sure You use prefix carefully
+Ex : -kill @user
+
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+"""
+com="""
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+[Ci]Common commands!!
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+-> ai [message] (ai chatbot)
+-> say [text]
+-> inviteall [invite online users in gc]
+-> global [@user]
+-> announce [your message]
+-> dice [.......]
+-> llocked [locked commands]
+-> coins [send fake coins]
+-> onmem [get online member list]
+-> jview [joins the chat as viewer]
+-> uid [get your id and chatid]
+-> getpfp [@user]
+-> joke [tell you a random joke]
+-> img [reply command on sticker]
+-> startvc [need cohost or host]
+-> endvc [need cohost or host]
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+"""
+ad="""
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+[C]Image commands
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+-> hug [me or @user]
+-> burger [for me or for @user]
+-> love [@user]
+-> hate [@user]
+-> slap [@user]
+-> trash [@user]
+-> propose [@user]
+-> kill [@user]
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+"""
+game ="""
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+[c]Rock paper scissors
+[c]Ex : -rps scissors
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+"""
+admin = """
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+[c]Admin commands
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+-> lock [command]
+-> unlock [command]
+-> remove [@user](remove as admin)
+-> add [@user](add as admin)
+-> ban [@user]
+-> unban [@user]
+-> join [invite and use]
+-> leave [leaves the gc]
+-> linkjoin [chat link]
+-> follow [me or @user]
+-> unfollow [me or @user]
+-> pm [me or @user]
+-> clear [need leader or curator]
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+"""
+
+bedit ="""
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+[c]∆bot~edit
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+[c]∆pfp
+[c]∆nick
+[C]✬━━━━━━━━━━━━━━━━━━━━━✮
+"""
+
+
+
+crash = """[11/08, 12:40] Krish: 🚫 DON'T TOUCH THIS MESSAGE ❌
+
+*This message was a mobile strucking message*
+
+✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘  ✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘  ✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘  ✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘͢͢⁦  ✘͢͢ۦོ͢✘͢͢ۦོ͢⇣͢✰͢↬ÂмRØ^^O̷ ꦿ⃕O̷↬ۦོ͢✰͢⇣͢✘
+[11/08, 12:41] Krish: 🚫 DON'T TOUCH THIS MESSAGE ❌"""
